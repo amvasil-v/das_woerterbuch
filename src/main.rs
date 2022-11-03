7c4d5093b6ef2ce0d3fe2ce0d0ead971f9d9a77b
Alexander Vasiliev <alexander.vasiliev@siemens.com>
2022-11-03 18:33:50 +0600

    add rotating exercise type

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 mod dictionary;
 mod game;
-mod words;
 mod game_reader;
+mod words;
+
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
 
 use crate::dictionary::*;
 use crate::game::*;
 use crate::game_reader::GameReader;
 
 #[allow(unused)]
+#[derive(EnumIter)]
 enum ExerciseType {
-    TranslateRuDe,
     SelectDe,
-    GuessNounArticle,
+    TranslateRuDe,
     SelectRu,
+    GuessNounArticle,
 }
 
-const EXERCISE_TYPE: ExerciseType = ExerciseType::SelectRu;
+//const EXERCISE_TYPE: ExerciseType = ExerciseType::SelectRu;
+const EXERCISE_MAX_COUNT: usize = 10;
 
 fn main() {
     let db = fill_database("woerterbuch.xlsx");
@@ -25,15 +30,34 @@ fn main() {
     results.update_with_db(&db);
     results.update_weights();
     let mut game = Game::new(db);
+    let mut exercise_count = 0usize;
+    let mut exersise_type_iter = ExerciseType::iter();
+    let mut exercise_type = exersise_type_iter.next().unwrap();
 
     println!("Type \"exit\" or press Ctrl-C to quit game");
     println!();
     loop {
-        let result = match EXERCISE_TYPE {
-            ExerciseType::TranslateRuDe => game.exercise_translate_to_de(&mut game_reader, &mut results),
+        exercise_count += 1;
+        if exercise_count >= EXERCISE_MAX_COUNT {
+            exercise_count = 0;
+            exercise_type = match exersise_type_iter.next() {
+                Some(t) => t,
+                None => {
+                    exersise_type_iter = ExerciseType::iter();
+                    exersise_type_iter.next().unwrap()
+                }
+            };
+        }
+
+        let result = match exercise_type {
+            ExerciseType::TranslateRuDe => {
+                game.exercise_translate_to_de(&mut game_reader, &mut results)
+            }
             ExerciseType::SelectDe => game.exercise_select_de(&mut game_reader, &mut results),
-            ExerciseType::GuessNounArticle => game.guess_noun_article(&mut game_reader, &mut results),
-            ExerciseType::SelectRu => game.exercise_select_ru(&mut game_reader, &mut results)
+            ExerciseType::GuessNounArticle => {
+                game.guess_noun_article(&mut game_reader, &mut results)
+            }
+            ExerciseType::SelectRu => game.exercise_select_ru(&mut game_reader, &mut results),
         };
 
         if let None = result {
